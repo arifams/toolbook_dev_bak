1e683773458b50462e84bf4539c3077154c8a3aa
ShanelPerera <sperera@mitrai.com>
2016-02-02 10:57:39 +0530

    Added Address Dto and missing project references

--- PI.Contract/DTOs/Customer/CustomerDto.cs
+++ PI.Contract/DTOs/Customer/CustomerDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PI.Contract.DTOs.Address;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,5 +14,6 @@ namespace PI.Contract.DTOs.Customer
         public string LastName { get; set; }
         public string MiddleName { get; set; }
         public string Email { get; set; }
+        public AddressDto CustomerAddress { get; set; }
     }
 }
