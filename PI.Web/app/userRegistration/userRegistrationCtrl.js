a5b9c576c4289499fc1795be364127cda5924718
damarasinghe <damarasinghe@mitrai.com>
2016-02-02 11:02:29 +0530

    added user Api controller

--- PI.Web/app/userRegistration/userRegistrationCtrl.js
+++ PI.Web/app/userRegistration/userRegistrationCtrl.js
@@ -3,13 +3,32 @@
 
 (function(app){
 
-    app.controller('userRegistrationCtrl', ['$http',function () {
+    app.controller('userRegistrationCtrl', [function ($q,registerUserService) {
         var vm = this;
         vm.register = function (user) {
-
-            window.alert("test");
+            //window.alert("test");
+            registerUserService.createUser(user)
+            .then(function (result)
+            {
+                console.log("success");
+            },
+            function (error) {
+                console.log("failed");
+            }
+            );
         };
     }]);
+
+    app.factory('registerUserService', function ($http, $q) {
+
+        var deferred = $q.defer();
+
+        this.createUser = function (user)
+        {
+             return $http.post('api/createConttroller', user);             
+
+        }
+    });
 
 })(angular.module('userRegistration', []));
 
