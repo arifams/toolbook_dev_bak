9522e1806f81115afa1d142de37915db6d175901
damarasinghe <damarasinghe@mitrai.com>
2016-02-03 13:53:11 +0530

    Added http.cors to wep.api to enable cross origin resource sharing

--- PI.Service/App_Start/WebApiConfig.cs
+++ PI.Service/App_Start/WebApiConfig.cs
@@ -11,6 +11,8 @@ namespace PI.Service
         {
             // Web API configuration and services
 
+            config.EnableCors();
+
             // Web API routes
             config.MapHttpAttributeRoutes();
 
--- PI.Service/Controllers/User/UserController.cs
+++ PI.Service/Controllers/User/UserController.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using PI.Contract;
 using PI.Contract.DTOs.Customer;
 using PI.Business;
+using System.Web.Http.Cors;
 
 namespace PI.Service.Controllers.User
 {
     public class UserController : ApiController
     {
+        [EnableCors(origins: "http://localhost:63874", headers: "*", methods: "*")]
         [HttpPost]
         public IHttpActionResult CreateUser([FromBody]CustomerDto customer)
         {
