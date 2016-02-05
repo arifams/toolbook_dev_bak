5512014db4b4801d59d2a965f0eb540655134b08
ShanelPerera <sperera@mitrai.com>
2016-02-05 19:30:14 +0530

    updated

--- PI.Web/app/userRegistration/userRegistrationCtrl.js
+++ PI.Web/app/userRegistration/userRegistrationCtrl.js
@@ -7,8 +7,8 @@
          
         return{
             createUser : function (newuser) {
-               // return $http.post('http://pibooking.azurewebsites.net/api/User/CreateUser', newuser);
-                return $http.post('http://localhost:5555/api/User/CreateUser', newuser);
+                return $http.post('http://pibooking.azurewebsites.net/api/User/CreateUser', newuser);
+               // return $http.post('http://localhost:5555/api/User/CreateUser', newuser);
 
         }
         };
@@ -46,8 +46,8 @@
                     vm.showEmailError = true;
                 }
                 else {
-                    // window.location = "http://pibookingservice.azurewebsites.net/app/index.html";
-                    window.location = "http://localhost:63874/app/index.html";
+                     window.location = "http://pibookingservice.azurewebsites.net/app/index.html";
+                    //window.location = "http://localhost:63874/app/index.html";
                 }
                 console.log("success");
                
