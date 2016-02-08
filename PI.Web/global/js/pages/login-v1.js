f04f371f3b71e8c90197808179a968d1e796bf69
damarasinghe <damarasinghe@mitrai.com>
2016-02-08 19:24:18 +0530

    edit validation messages

--- PI.Web/global/js/pages/login-v1.js
+++ PI.Web/global/js/pages/login-v1.js
@@ -504,7 +504,7 @@ $(function() {
                        
                     },
                     street: {
-                        required: 'Enter Street',
+                        required: 'Enter Street Address1',
                         
                     },
                     city: {
@@ -556,7 +556,7 @@ $(function() {
                     },
                     Password: result.password
                 }
-                var jqxhr = $.post('http://localhost:5555/api/User/createuser', newUser)
+                var jqxhr = $.post('https://pibooking.azurewebsites.net/api/User/createuser', newUser)
              .success(function () {
                  var loc = jqxhr.getResponseHeader('Location');
                  var responce = jqxhr.responseJSON;
