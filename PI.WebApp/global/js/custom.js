07a54cd831bf33b42ea1a2ae7c6506076a8df8f2
ShanelPerera <sperera@mitrai.com>
2016-02-25 12:17:02 +0530

    Updated

--- PI.WebApp/global/js/custom.js
+++ PI.WebApp/global/js/custom.js
@@ -5,5 +5,5 @@ var serverBaseUrl = 'http://localhost:5555';
 //var serverBaseUrl = 'http://pibooking.azurewebsites.net';
 
 // Web base url
-var webBaseUrl = 'http://localhost:63874';
+var webBaseUrl = 'http://localhost:49995';
 //var webBaseUrl = 'http://pibookingservice.azurewebsites.net';
