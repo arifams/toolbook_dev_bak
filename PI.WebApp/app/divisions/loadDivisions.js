98bd523d8944ac50ce9efe2194e1deec6a5ae0c2
Harshan1 <hpremarathna@mitrai.com>
2016-02-25 20:22:16 +0530

    Change load division page to use smart table

--- PI.WebApp/app/divisions/loadDivisions.js
+++ PI.WebApp/app/divisions/loadDivisions.js
@@ -11,12 +11,15 @@
     });
 
     //get all costSenters for the given user
-    app.factory('loadAllCostCenters', function ($http, $window) {
+    app.factory('loadCostCenters', function ($http, $window) {
         return {
-            loadCostCenters: function () {
-                return $http.get(serverBaseUrl + '/api/Company/GetAllCostCenters', {
+            loadCostCenters: function (userId,searchText,costCenter,type) {
+                return $http.get(serverBaseUrl + '/api/Company/GetAllDivisionsByFliter', {
                     params: {
-                        userId: $window.localStorage.getItem('userGuid') //$localStorage.userGuid
+                        userId: userId,
+                        searchtext: searchText,
+                        costCenter: costCenter,
+                        type: type
                     }
                 });
             }
@@ -24,206 +27,110 @@
 
     });
 
-    app.factory('loadDivisionService', function ($http, $q, $log, $rootScope) {        
-              
-        var baseUrl = serverBaseUrl + '/api/Company/GetAllDivisionsByFliter';
-        
-        var service = {
-            data: {
-                currentDivision: {},
-                divisions: [],
-                selected: [],
-                totalPages: 0,
+    app.controller('loadDivisionsCtrl', function ($scope, $location, loadCostCenters, divisionManagmentService, $routeParams, $log, $window) {
 
-                filterOptions: {
-                    filterText: '',
-                    externalFilter: 'searchText',
-                    useExternalFilter: true
-                },
-                sortOptions: {
-                    fields: ["Id"],
-                    directions: ["desc"]
-                },
-                pagingOptions: {
-                    pageSizes: [10, 20, 50, 100],
-                    pageSize: 10,
-                    currentPage: 1
-                },
-                searchTypes: {
-                    costCenter: 0,
-                    type:''
-                },
-                user: {
-                    userId:''
-                }
-            },
+        var userId = $window.localStorage.getItem('userGuid');
+        var costCenter = $scope.costcenter == undefined ? 0 : $scope.costcenter;
+        var type = $scope.status == undefined ? 0 : $scope.status;
+        // Passed filterby and status as 0. Need to change this
 
-            find: function () {
-                var params = {
-                    costCenter: service.data.searchTypes.costCenter,
-                    type: service.data.searchTypes.type,
-                    userId:service.data.user.userId,
-                    searchtext: service.data.filterOptions.filterText,                    
-                    page: service.data.pagingOptions.currentPage,
-                    pageSize: service.data.pagingOptions.pageSize,
-                    sortBy: service.data.sortOptions.fields[0],
-                    sortDirection: service.data.sortOptions.directions[0]
-                    
-                };
-
-                var deferred = $q.defer();
-                $http.get(baseUrl, { params: params })
-                .success(function (data) {
-                    service.data.divisions = data;
-                    deferred.resolve(data);
-                }).error(function () {
-                    deferred.reject();
-                });
-                return deferred.promise;
-            }
-        }
-
-        service.find();
-        return service;
-    });
-
-
-    app.controller('loadDivisionsCtrl', function ($scope, $location, loadAllCostCenters, loadDivisionService, divisionManagmentService, $routeParams, $log, $window) {
-    $scope.data = loadDivisionService.data;
-    $scope.data.user.userId = $window.localStorage.getItem('userGuid')
-
-
-        loadAllCostCenters.loadCostCenters()
-               .then(function successCallback(responce) {
+        loadCostCenters.loadCostCenters(userId, $scope.searchText, costCenter, type)
+        .then(function successCallback(responce) {
                   
-                   $scope.costCenters = responce.data;                   
+            $scope.rowCollection = responce.data;
 
-               }, function errorCallback(response) {
-                   //todo
-               });
-
-
-    $scope.$watch('data.sortOptions', function (newVal, oldVal) {
-        $log.log("sortOptions changed: " + newVal);
-        if (newVal !== oldVal) {
-            $scope.data.pagingOptions.currentPage = 1;
-            loadDivisionService.find();
-        }
-    }, true);
-
-    //$scope.$watch('data.filterOptions', function (newVal, oldVal) {
-    //    $log.log("filters changed: " + newVal);
-    //    if (newVal !== oldVal) {
-    //        $scope.data.pagingOptions.currentPage = 1;
-    //        loadDivisionService.find();
-    //    }
-    //}, true);
-
-    $scope.$watch('data.pagingOptions', function (newVal, oldVal) {
-        $log.log("page changed: " + newVal);
-        if (newVal !== oldVal) {
-            loadDivisionService.find();
-        }
-    }, true);
+        }, function errorCallback(response) {
+            //todo
+        });
    
 
-    $scope.gridOptions = {
-        data: 'data.divisions.content',
-        showFilter: false,
-        multiSelect: false,
-        selectedItems: $scope.data.selected,
-        enablePaging: true,
-        showFooter: true,
-        totalServerItems: 'data.divisions.totalRecords',
-        pagingOptions: $scope.data.pagingOptions,
-        filterOptions: $scope.data.filterOptions,
-        useExternalSorting: true,
-        sortInfo: $scope.data.sortOptions,
-        plugins: [new ngGridFlexibleHeightPlugin()],
-        columnDefs: [
-                    { field: 'id', displayName: 'DivisionId', visible: false },
-                    { field: 'name', displayName: 'Division' },
-                    { field: 'assignedDivisionsForGrid', displayName: 'Assigned Cost Centers', sortable: false },
-                    { field: 'numberOfUsers', displayName: 'Number Users', sortable: false },
-                    { field: 'status', displayName: 'Status' },
-                    {
-                        field: 'editLink', displayName: 'Edit', enableCellEdit: false,
-                        cellTemplate: '<a href="#/saveDivision/{{row.entity.id}}" class="edit btn btn-sm btn-default" href="javascript:;"><i class="icon-note"></i></a>', sortable: false
-                    },
-                    {
-                        field: 'deleteLink', displayName: 'Delete', enableCellEdit: false,
-                        cellTemplate: '<a ng-click="deleteById(row)" class="delete btn btn-sm btn-danger" href="javascript:;"><i class="icons-office-52"></i></a>', sortable: false
-                    }
-        ],
-        afterSelectionChange: function (selection, event) {
-            // $log.log("selection: " + selection.entity.CustomerID);
-            // $location.path("comments/" + selection.entity.commentId);
-        }
-    };
+    //$scope.gridOptions = {
+    //    data: 'data.divisions.content',
+    //    showFilter: false,
+    //    multiSelect: false,
+    //    selectedItems: $scope.data.selected,
+    //    enablePaging: true,
+    //    showFooter: true,
+    //    totalServerItems: 'data.divisions.totalRecords',
+    //    pagingOptions: $scope.data.pagingOptions,
+    //    filterOptions: $scope.data.filterOptions,
+    //    useExternalSorting: true,
+    //    sortInfo: $scope.data.sortOptions,
+    //    plugins: [new ngGridFlexibleHeightPlugin()],
+    //    columnDefs: [
+    //                { field: 'id', displayName: 'DivisionId', visible: false },
+    //                { field: 'name', displayName: 'Division' },
+    //                { field: 'assignedDivisionsForGrid', displayName: 'Assigned Cost Centers', sortable: false },
+    //                { field: 'numberOfUsers', displayName: 'Number Users', sortable: false },
+    //                { field: 'status', displayName: 'Status' },
+    //                {
+    //                    field: 'editLink', displayName: 'Edit', enableCellEdit: false,
+    //                    cellTemplate: '<a href="#/saveDivision/{{row.entity.id}}" class="edit btn btn-sm btn-default" href="javascript:;"><i class="icon-note"></i></a>', sortable: false
+    //                },
+    //                {
+    //                    field: 'deleteLink', displayName: 'Delete', enableCellEdit: false,
+    //                    cellTemplate: '<a ng-click="deleteById(row)" class="delete btn btn-sm btn-danger" href="javascript:;"><i class="icons-office-52"></i></a>', sortable: false
+    //                }
+    //    ],
+    //    afterSelectionChange: function (selection, event) {
+    //        // $log.log("selection: " + selection.entity.CustomerID);
+    //        // $location.path("comments/" + selection.entity.commentId);
+    //    }
+    //};
 
-    $scope.searchDivisions = function () {
+    //$scope.searchDivisions = function () {
 
-        $scope.data.filterOptions.filterText = $scope.searchText;
-        loadDivisionService.find();
-        //loadDivisionService.find($scope.filterOptions.filterText);
-    };
+    //    $scope.data.filterOptions.filterText = $scope.searchText;
+    //    //loadDivisionService.find();
+    //};
 
-    $scope.selectActiveDivisions = function () {
+    //$scope.selectActiveDivisions = function () {
 
-        if (angular.isUndefined($scope.status)) {
+    //    if (angular.isUndefined($scope.status)) {
 
-            $scope.data.searchTypes.type = '';
-        }else {
-            $scope.data.searchTypes.type = $scope.status;
-        }       
-        loadDivisionService.find();
-    };
+    //        $scope.data.searchTypes.type = '';
+    //    }else {
+    //        $scope.data.searchTypes.type = $scope.status;
+    //    }       
+    //    //loadDivisionService.find();
+    //};
 
-    $scope.selectDivisionforCostCenter = function () {
-        if (angular.isUndefined($scope.costcenter)) {
+    //$scope.selectDivisionforCostCenter = function () {
+    //    if (angular.isUndefined($scope.costcenter)) {
 
-            $scope.data.searchTypes.costCenter = 0;
-        } else {
+    //        $scope.data.searchTypes.costCenter = 0;
+    //    } else {
                 
-            $scope.data.searchTypes.costCenter = $scope.costcenter;
-        }
+    //        $scope.data.searchTypes.costCenter = $scope.costcenter;
+    //    }
       
-        loadDivisionService.find();
-    }
+    //    //loadDivisionService.find();
+    //}
 
-
-    $scope.deleteById = function (row) {
+    //$scope.deleteById = function (row) {
        
-        var r = confirm("Do you want to delete the record?");
-        if (r == true) {
-            divisionManagmentService.deleteDivision({ Id: row.entity.id })
-                        .success(function (response) {
-                            debugger;
-                            if (response == 1) {
-                                debugger;
-                                // remove($scope.gridOptions.data, 'id', row.entity.id);
+    //    var r = confirm("Do you want to delete the record?");
+    //    if (r == true) {
+    //        divisionManagmentService.deleteDivision({ Id: row.entity.id })
+    //                    .success(function (response) {
+    //                        debugger;
+    //                        if (response == 1) {
+    //                            debugger;
+    //                            // remove($scope.gridOptions.data, 'id', row.entity.id);
 
-                                angular.forEach($scope.data.divisions.content, function (index, result) {
-                                    if (index.id == row.entity.id) {
-                                        $scope.data.divisions.content.splice(index, 1);
-                                    }
-                                })
+    //                            angular.forEach($scope.data.divisions.content, function (index, result) {
+    //                                if (index.id == row.entity.id) {
+    //                                    $scope.data.divisions.content.splice(index, 1);
+    //                                }
+    //                            })
 
-                            }
-                        })
-                       .error(function () {
-                       })
-        }
-    };
+    //                        }
+    //                    })
+    //                   .error(function () {
+    //                   })
+    //    }
+    //};
 
-    // parse the gridData array to find the object with Id
-    function remove(array, property, value) {
-        angular.forEach(array, function (index, result) {
-            if (result[property] == value) {
-                array.splice(index, 1);
-            }
-        });
-    };
 
 });
 
