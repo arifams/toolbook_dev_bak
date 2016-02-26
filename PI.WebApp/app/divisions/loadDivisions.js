518b2bc058f7ce9936cf210742f28a21c7039095
Harshan1 <hpremarathna@mitrai.com>
2016-02-26 09:30:48 +0530

    Fixed issues of Smart table in load division

--- PI.WebApp/app/divisions/loadDivisions.js
+++ PI.WebApp/app/divisions/loadDivisions.js
@@ -11,9 +11,25 @@
     });
 
     //get all costSenters for the given user
-    app.factory('loadCostCenters', function ($http, $window) {
+    app.factory('loadAllCostCenters', function ($http, $window) {
         return {
-            loadCostCenters: function (userId,searchText,costCenter,type) {
+            loadCostCenters: function () {
+                return $http.get(serverBaseUrl + '/api/Company/GetAllCostCenters', {
+                    params: {
+                        userId: $window.localStorage.getItem('userGuid') 
+                    }
+                });
+            }
+        }
+
+    });
+
+    app.factory('loadDivisionService', function ($http, $q, $log, $rootScope) {
+
+        var baseUrl = serverBaseUrl + '/api/Company/GetAllDivisionsByFliter';
+
+        return {
+            find: function (userId, searchText, costCenter, type) {
                 return $http.get(serverBaseUrl + '/api/Company/GetAllDivisionsByFliter', {
                     params: {
                         userId: userId,
@@ -24,110 +40,78 @@
                 });
             }
         }
-
     });
 
-    app.controller('loadDivisionsCtrl', function ($scope, $location, loadCostCenters, divisionManagmentService, $routeParams, $log, $window) {
 
-        var userId = $window.localStorage.getItem('userGuid');
-        var costCenter = $scope.costcenter == undefined ? 0 : $scope.costcenter;
-        var type = $scope.status == undefined ? 0 : $scope.status;
-        // Passed filterby and status as 0. Need to change this
+    app.controller('loadDivisionsCtrl', function ($scope, $location, loadAllCostCenters, loadDivisionService, divisionManagmentService, $routeParams, $log, $window) {
+        
+        // Load all cost centers
+        loadAllCostCenters.loadCostCenters()
+            .then(function successCallback(responce) {
 
-        loadCostCenters.loadCostCenters(userId, $scope.searchText, costCenter, type)
-        .then(function successCallback(responce) {
-                  
-            $scope.rowCollection = responce.data.content;
-           
-        }, function errorCallback(response) {
-            //todo
-        });
-   
+                $scope.costCenters = responce.data;
 
-    //$scope.gridOptions = {
-    //    data: 'data.divisions.content',
-    //    showFilter: false,
-    //    multiSelect: false,
-    //    selectedItems: $scope.data.selected,
-    //    enablePaging: true,
-    //    showFooter: true,
-    //    totalServerItems: 'data.divisions.totalRecords',
-    //    pagingOptions: $scope.data.pagingOptions,
-    //    filterOptions: $scope.data.filterOptions,
-    //    useExternalSorting: true,
-    //    sortInfo: $scope.data.sortOptions,
-    //    plugins: [new ngGridFlexibleHeightPlugin()],
-    //    columnDefs: [
-    //                { field: 'id', displayName: 'DivisionId', visible: false },
-    //                { field: 'name', displayName: 'Division' },
-    //                { field: 'assignedDivisionsForGrid', displayName: 'Assigned Cost Centers', sortable: false },
-    //                { field: 'numberOfUsers', displayName: 'Number Users', sortable: false },
-    //                { field: 'status', displayName: 'Status' },
-    //                {
-    //                    field: 'editLink', displayName: 'Edit', enableCellEdit: false,
-    //                    cellTemplate: '<a href="#/saveDivision/{{row.entity.id}}" class="edit btn btn-sm btn-default" href="javascript:;"><i class="icon-note"></i></a>', sortable: false
-    //                },
-    //                {
-    //                    field: 'deleteLink', displayName: 'Delete', enableCellEdit: false,
-    //                    cellTemplate: '<a ng-click="deleteById(row)" class="delete btn btn-sm btn-danger" href="javascript:;"><i class="icons-office-52"></i></a>', sortable: false
-    //                }
-    //    ],
-    //    afterSelectionChange: function (selection, event) {
-    //        // $log.log("selection: " + selection.entity.CustomerID);
-    //        // $location.path("comments/" + selection.entity.commentId);
-    //    }
-    //};
-
-    //$scope.searchDivisions = function () {
-
-    //    $scope.data.filterOptions.filterText = $scope.searchText;
-    //    //loadDivisionService.find();
-    //};
-
-    //$scope.selectActiveDivisions = function () {
-
-    //    if (angular.isUndefined($scope.status)) {
-
-    //        $scope.data.searchTypes.type = '';
-    //    }else {
-    //        $scope.data.searchTypes.type = $scope.status;
-    //    }       
-    //    //loadDivisionService.find();
-    //};
-
-    //$scope.selectDivisionforCostCenter = function () {
-    //    if (angular.isUndefined($scope.costcenter)) {
-
-    //        $scope.data.searchTypes.costCenter = 0;
-    //    } else {
-                
-    //        $scope.data.searchTypes.costCenter = $scope.costcenter;
-    //    }
-      
-    //    //loadDivisionService.find();
-    //}
-
-    $scope.deleteById = function (row) {
-       
-        var r = confirm("Do you want to delete the record?");
-        if (r == true) {
-            divisionManagmentService.deleteDivision({ Id: row.id })
-            .success(function (response) {
-
-                if (response == 1) {
-                    var index = $scope.rowCollection.indexOf(row);
-                    if (index !== -1) {
-                        $scope.rowCollection.splice(index, 1);
-                    }
-                }
-            })
-            .error(function () {
-                debugger;
+            }, function errorCallback(response) {
+                //todo
             });
+
+        $scope.itemsByPage = 2; // Set page size
+        $scope.rowCollection = [];
+        // Add dumy record, since data loading is async.
+        $scope.rowCollection.push(1);
+
+        $scope.searchDivisions = function () {
+
+            // Get values from view.
+            var userId = $window.localStorage.getItem('userGuid');
+            var costCenter = ($scope.costcenter == undefined || $scope.costcenter == "") ? 0 : $scope.costcenter;
+            var type = ($scope.status == undefined || $scope.status == "") ? 0 : $scope.status;
+            var searchText = $scope.searchText;
+            
+            loadDivisionService.find(userId, searchText, costCenter, type)
+                .then(function successCallback(responce) {
+
+                    $scope.rowCollection = responce.data.content;
+
+                }, function errorCallback(response) {
+                    //todo
+                });
+        };
+
+        // Call search function in page load.
+        $scope.searchDivisions();
+
+        $scope.selectActiveDivisions = function () {
+
+            $scope.searchDivisions();
+        };
+
+        $scope.selectDivisionforCostCenter = function () {
+  
+            $scope.searchDivisions();
         }
-    };
 
+        // Delete row.
+        $scope.deleteById = function (row) {
 
-});
+            var r = confirm("Do you want to delete the record?");
+            if (r == true) {
+                divisionManagmentService.deleteDivision({ Id: row.id })
+                .success(function (response) {
+
+                    if (response == 1) {
+                        var index = $scope.rowCollection.indexOf(row);
+                        if (index !== -1) {
+                            $scope.rowCollection.splice(index, 1);
+                        }
+                    }
+                })
+                .error(function () {
+                    debugger;
+                });
+            }
+        };
+
+    });
 
 })(angular.module('newApp'));
