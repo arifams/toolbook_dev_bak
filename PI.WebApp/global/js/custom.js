b4e99d086bb07bd484b34e7a69c0399b260d748b
Harshan1 <hpremarathna@mitrai.com>
2016-02-26 12:25:30 +0530

    Updated web.config and custom.js to match for production.

--- PI.WebApp/global/js/custom.js
+++ PI.WebApp/global/js/custom.js
@@ -1,9 +1,9 @@
 //****************** YOUR CUSTOMIZED JAVASCRIPT **********************//
 
 // Service base url
-var serverBaseUrl = 'http://localhost:5555';
-//var serverBaseUrl = 'http://pibooking.azurewebsites.net';
+//var serverBaseUrl = 'http://localhost:5555';
+var serverBaseUrl = 'http://pibooking.azurewebsites.net';
 
 // Web base url
-var webBaseUrl = 'http://localhost:49995';
-//var webBaseUrl = 'http://pibookingservice.azurewebsites.net';
+//var webBaseUrl = 'http://localhost:49995';
+var webBaseUrl = 'http://pibookingservice.azurewebsites.net';
