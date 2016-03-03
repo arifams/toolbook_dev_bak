9b541f52130bea899215035c32908829a60202a9
Harshan1 <hpremarathna@mitrai.com>
2016-03-03 17:29:56 +0530

    Added roles name to seed method.

--- PI.Data/Migrations/Configuration.cs
+++ PI.Data/Migrations/Configuration.cs
@@ -83,9 +83,11 @@ namespace PI.Data.Migrations
 
             if (roleManager.Roles.Count() == 0)
             {
-                roleManager.Create(new IdentityRole { Name = "SuperAdmin" });
                 roleManager.Create(new IdentityRole { Name = "Admin" });
-                roleManager.Create(new IdentityRole { Name = "User" });
+                roleManager.Create(new IdentityRole { Name = "BusinessOwner" });
+                roleManager.Create(new IdentityRole { Name = "Manager" });
+                roleManager.Create(new IdentityRole { Name = "Supervisor" });
+                roleManager.Create(new IdentityRole { Name = "Operator" });
             }
 
             var adminUser = manager.FindByName("SuperPowerUser");
