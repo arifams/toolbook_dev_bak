09ff4b66fc6097affa8f99a8fc0a2cf5dfc630dc
Harshan1 <hpremarathna@mitrai.com>
2016-03-04 08:34:32 +0530

    Revert all changes done to user login js which was validate token seperatly.

--- PI.WebApp/app/userLogin/userLoginCtrl.js
+++ PI.WebApp/app/userLogin/userLoginCtrl.js
@@ -1,37 +1,27 @@
 ﻿'use strict';
 
 
-(function (app) {    
+(function (app) {
 
     app.factory('userManager', function ($http) {
         return {
             loginUser: function (newuser, url) {
-                
+
                 return $http.post(serverBaseUrl + '/' + url, newuser);
             }
         };
 
     });
 
-    app.factory('validateUserToken', function ($http) {
-        return {
-            validateUserToken: function (user, url) {
-
-                return $http.post(serverBaseUrl + '/' + url, user);
-            }
-        };
-
-    });
-
-    app.controller('userLoginCtrl', ['userManager', '$window', '$cookieStore','$scope','validateUserToken',
-    function (userManager, $window, $cookieStore, $scope, validateUserToken) {
-        var vm = this;     
+    app.controller('userLoginCtrl', ['userManager', '$window', '$cookieStore', '$scope',
+    function (userManager, $window, $cookieStore, $scope) {
+        var vm = this;
         //$localStorage.userGuid = '';
         $window.localStorage.setItem('userGuid', '');
         vm.loginInvalid = false;
         vm.invalidToken = false;
         vm.isEmailConfirm = false;
-       
+
         vm.isSentPasswordResetMail = false;
         vm.passwordResetError = false;
 
@@ -42,11 +32,27 @@
         if (loggedusername && loggedpassword) {
             $scope.user.username = loggedusername;
             $scope.user.password = loggedpassword;
-       }
-
-        var userId = '', code = '', isConfirmEmail = false;
+        }
 
         vm.isConfirmEmail = function () {
+
+            if (window.location.search != "") {
+                // Show email confirm message.
+                vm.emailConfirmationMessage = "To confirm email address, please login using your username and password";
+                vm.isEmailConfirm = true;
+
+            }
+
+        };
+
+        vm.login = function (user) {
+
+            debugger;
+            if (vm.rememberme == true) {
+                $cookieStore.put('username', user.username);
+                $cookieStore.put('password', user.password);
+            }
+
 
             if (window.location.search != "") {
 
@@ -69,75 +75,10 @@
                     return;
                 }
 
-                userId = userIdKeyValue[1];
-                code = codeKeyValue[1];
-
-                var userToken = {
-                    userId: userId,
-                    code: code
-                };
-
-                // Check token is valid.
-                validateUserToken.validateUserToken(userToken, 'api/accounts/ValidateUserToken')
-                    .then(function (returnedResult) {
-                        
-                        if (returnedResult.data) {
-                            isConfirmEmail = true;
-
-                            // Show email confirm message.
-                            vm.emailConfirmationMessage = "To confirm email address, please login using your username and password";
-                            vm.isEmailConfirm = true;
-                        }
-                        else {
-                            vm.emailConfirmationMessage = "Token is Expired";
-                            vm.isEmailConfirm = true;
-                            return;
-                        }
-                });
-
-                
-
+                user.userId = userIdKeyValue[1];
+                user.code = codeKeyValue[1];
+                user.isConfirmEmail = true;
             }
-
-        };
-
-        vm.login = function (user) {
-
-            if (vm.rememberme==true) {
-                $cookieStore.put('username', user.username);
-                $cookieStore.put('password', user.password);
-            }
-         
-            // window location search...........................
-            //if (window.location.search != "") {
-
-            //    var splittedValues = window.location.search.replace("?", "").split('&');
-
-            //    if (splittedValues.length != 2 || splittedValues[0].split('=').length != 2 || splittedValues[1].split('=').length != 2) {
-            //        vm.emailConfirmationMessage = "Confirmation URL link is not properly formatted. Please resend the confirmation URL";
-            //        return;
-            //    }
-
-            //    var userIdKeyValue = splittedValues[0].split('=');
-            //    var codeKeyValue = splittedValues[1].split('=');
-
-            //    if (userIdKeyValue[0] != 'userId'){
-            //        vm.emailConfirmationMessage = "Confirmation URL link is not properly formatted. Please resend the confirmation URL";
-            //        return;
-            //    }
-            //    if (codeKeyValue[0] != 'code') {
-            //        vm.emailConfirmationMessage = "Confirmation URL link is not properly formatted. Please resend the confirmation URL";
-            //        return;
-            //    }
-
-            //    user.userId = userIdKeyValue[1];
-            //    user.code = codeKeyValue[1];
-            //    user.isConfirmEmail = true;
-            //}
-
-            user.userId = userId;
-            user.code = code;
-            user.isConfirmEmail = isConfirmEmail;
 
             userManager.loginUser(user, 'api/accounts/LoginUser')
              .then(function (returnedResult) {
@@ -162,7 +103,7 @@
                  else if (returnedResult.data.result == "-11") {
                      vm.loginInvalid = true;
                      vm.loginInvalidMessage = "You must have a confirmed email to log in";
-                    $cookieStore.remove('username');
+                     $cookieStore.remove('username');
                      $cookieStore.remove('password');
                  }
              },
@@ -186,19 +127,19 @@
 
                 console.log("failed");
             });
-           
+
         };
 
         vm.isConfirmEmail();
 
         vm.resetForgetPassword = function () {
 
-         vm.pwdReset.templateLink = '<html><head>    <title></title></head><body>    <p>        <img alt="" src="http://www.parcelinternational.nl/assets/Uploads/_resampled/SetWidth495-id-parcel-big.jpg"             style="width: 200px; height: 200px; float: right;" />    </p><div>        <h4 style="text-align: justify;">            &nbsp;        </h4><div style="background:#eee;border:1px solid #ccc;padding:5px 10px;">            <span style="font-family:verdana,geneva,sans-serif;">                <span style="color:#0000CD;">                    <span style="font-size:28px;">                        Password Reset                    </span>                </span>            </span>        </div><p style="text-align: justify;">&nbsp;</p><h4 style="text-align: justify;">            &nbsp;        </h4><h4 style="text-align: justify;">            <span style="font-size:12px;">                <span style="font-family:verdana,geneva,sans-serif;">                    Dear <strong>FirstName &nbsp;LastName, &nbsp;</strong>                </span>            </span>        </h4><h4 style="text-align: justify;">            <br /><span style="font-size:12px;">                <span style="font-family:verdana,geneva,sans-serif;">                    <strong>                        We received a request to reset the password associated with this email address.                        If you made this request, please follow the instructions below.                        &nbsp;&nbsp;                    </strong>                </span>            </span>        </h4><h4 style="text-align: justify;">            <span style="font-size:12px;">                <span style="font-family:verdana,geneva,sans-serif;">                    <strong>If you did not request to have your password reset, you can safetly ignore this email.</strong>                </span>            </span>        </h4><h4 style="text-align: justify;">            <span style="font-size:12px;">                <span style="font-family:verdana,geneva,sans-serif;">                    <strong>Please click this &nbsp;ActivationURL&nbsp;</strong>                </span>            </span>        </h4>        <h4 style="text-align: justify;">            <span style="font-size:12px;">                <span style="font-family:verdana,geneva,sans-serif;">                    <strong>IMPORTANT! This link is valid for 24 hours only. &nbsp;&nbsp;</strong>                </span>            </span>        </h4>        <h4 style="text-align: justify;">            <span style="font-size:12px;">                <span style="font-family:verdana,geneva,sans-serif;">                    <strong>Should you have any questions or concerns, please contact Parcel International helpdesk for support &nbsp;</strong>                </span>            </span>        </h4>        <h4 style="text-align: justify;">            <span style="font-size:12px;">                <span style="font-family:verdana,geneva,sans-serif;">                    <strong>                        *** This is an automatically generated email, please do not reply ***                    </strong>                </span>            </span>        </h4>        <h4 style="text-align: justify;">&nbsp;</h4><h4 style="text-align: justify;">            <strong>                <span style="font-size:12px;">                    <span style="font-family:verdana,geneva,sans-serif;">Thank You, </span>                </span>            </strong>        </h4><h4 style="text-align: justify;">            <strong>                <span style="font-size:12px;">                    <span style="font-family:verdana,geneva,sans-serif;">Parcel International Team</span>                </span>            </strong>        </h4>    </div></body></html>';
+            vm.pwdReset.templateLink = '<html><head>    <title></title></head><body>    <p>        <img alt="" src="http://www.parcelinternational.nl/assets/Uploads/_resampled/SetWidth495-id-parcel-big.jpg"             style="width: 200px; height: 200px; float: right;" />    </p><div>        <h4 style="text-align: justify;">            &nbsp;        </h4><div style="background:#eee;border:1px solid #ccc;padding:5px 10px;">            <span style="font-family:verdana,geneva,sans-serif;">                <span style="color:#0000CD;">                    <span style="font-size:28px;">                        Password Reset                    </span>                </span>            </span>        </div><p style="text-align: justify;">&nbsp;</p><h4 style="text-align: justify;">            &nbsp;        </h4><h4 style="text-align: justify;">            <span style="font-size:12px;">                <span style="font-family:verdana,geneva,sans-serif;">                    Dear <strong>FirstName &nbsp;LastName, &nbsp;</strong>                </span>            </span>        </h4><h4 style="text-align: justify;">            <br /><span style="font-size:12px;">                <span style="font-family:verdana,geneva,sans-serif;">                    <strong>                        We received a request to reset the password associated with this email address.                        If you made this request, please follow the instructions below.                        &nbsp;&nbsp;                    </strong>                </span>            </span>        </h4><h4 style="text-align: justify;">            <span style="font-size:12px;">                <span style="font-family:verdana,geneva,sans-serif;">                    <strong>If you did not request to have your password reset, you can safetly ignore this email.</strong>                </span>            </span>        </h4><h4 style="text-align: justify;">            <span style="font-size:12px;">                <span style="font-family:verdana,geneva,sans-serif;">                    <strong>Please click this &nbsp;ActivationURL&nbsp;</strong>                </span>            </span>        </h4>        <h4 style="text-align: justify;">            <span style="font-size:12px;">                <span style="font-family:verdana,geneva,sans-serif;">                    <strong>IMPORTANT! This link is valid for 24 hours only. &nbsp;&nbsp;</strong>                </span>            </span>        </h4>        <h4 style="text-align: justify;">            <span style="font-size:12px;">                <span style="font-family:verdana,geneva,sans-serif;">                    <strong>Should you have any questions or concerns, please contact Parcel International helpdesk for support &nbsp;</strong>                </span>            </span>        </h4>        <h4 style="text-align: justify;">            <span style="font-size:12px;">                <span style="font-family:verdana,geneva,sans-serif;">                    <strong>                        *** This is an automatically generated email, please do not reply ***                    </strong>                </span>            </span>        </h4>        <h4 style="text-align: justify;">&nbsp;</h4><h4 style="text-align: justify;">            <strong>                <span style="font-size:12px;">                    <span style="font-family:verdana,geneva,sans-serif;">Thank You, </span>                </span>            </strong>        </h4><h4 style="text-align: justify;">            <strong>                <span style="font-size:12px;">                    <span style="font-family:verdana,geneva,sans-serif;">Parcel International Team</span>                </span>            </strong>        </h4>    </div></body></html>';
 
             userManager.loginUser(vm.pwdReset, 'api/accounts/ResetForgetPassword')
              .then(function (returnedResult) {
                  debugger;
-                 
+
                  if (returnedResult.data == "1") {
                      vm.passwordResetError = false;
                      vm.isSentPasswordResetMail = true;
@@ -223,5 +164,5 @@
     }]);
 
 
-})(angular.module('userLogin', ['ngMessages','ngCookies']));
+})(angular.module('userLogin', ['ngMessages', 'ngCookies']));
 
