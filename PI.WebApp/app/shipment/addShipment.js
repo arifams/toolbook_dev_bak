4f12c8209614b166a89bdf9418ab867c89a9a3ef
Harshan1 <hpremarathna@mitrai.com>
2016-03-07 14:03:34 +0530

    Added abstract service and controller for shipment

--- PI.WebApp/app/shipment/addShipment.js
+++ PI.WebApp/app/shipment/addShipment.js
@@ -1,1 +1,14 @@
-﻿
+﻿'use strict';
+
+(function (app) {
+
+    app.controller('addShipmentCtrl', ['$location', '$window', 'shipmentFactory', function ($location, $window, userManagementFactory) {
+        var vm = this;
+        vm.user = {};
+
+
+
+    }]);
+
+
+})(angular.module('newApp'));
--- PI.WebApp/app/shipment/shipmentService.js
+++ PI.WebApp/app/shipment/shipmentService.js
@@ -1,1 +1,17 @@
-﻿
+﻿'use strict';
+
+(function (app) {
+
+    app.factory('shipmentFactory', ['$http', '$routeParams', '$window', function ($http, $routeParams, $window) {
+
+        return {
+            
+        }
+
+        // Implement the functions.
+
+       
+
+    }]);
+
+})(angular.module('newApp'));
