2ba77e411a96bf00d47917bf2c31d80cb46f9715
ShanelPerera <sperera@mitrai.com>
2016-03-09 17:25:46 +0530

    Added Interface for SIS Integration

---
+++ PI.Business/SISIntegrationManager.cs
@@ -0,0 +1,38 @@
+﻿using PI.Contract.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI.Business
+{
+    public class SISIntegrationManager : ICarrierIntegrationManager
+    {
+
+        string ICarrierIntegrationManager.GetRateSheetForShipment(string URL)
+        {
+            throw new NotImplementedException();
+        }
+
+        string ICarrierIntegrationManager.SubmitShipment(string xmlDetail)
+        {
+            throw new NotImplementedException();
+        }
+
+        string ICarrierIntegrationManager.DeleteShipment(string shipmentCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        string ICarrierIntegrationManager.GetShipmentStatus(string URL)
+        {
+            throw new NotImplementedException();
+        }
+
+        string ICarrierIntegrationManager.TrackAndTraceShipment(string URL)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
