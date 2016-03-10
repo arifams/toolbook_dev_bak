d955c649d3580cd319abc0e1a8eacca51428de3a
damarasinghe <damarasinghe@mitrai.com>
2016-03-10 17:55:45 +0530

    added new controller for shipment Management

---
+++ PI.Business/ShipmentsManagement.cs
@@ -0,0 +1,28 @@
+﻿using PI.Contract.DTOs.RateSheets;
+using PI.Contract.DTOs.Shipment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI.Business
+{
+    public class ShipmentsManagement
+    {
+        public ShipmentcostList GetRateSheet(ShipmentDto currentShipment)
+        {
+            SISIntegrationManager sisManager = new SISIntegrationManager();
+            RateSheetParametersDto currentRateSheetDetails = new RateSheetParametersDto();
+
+           
+
+
+
+
+
+            return sisManager.GetRateSheetForShipment(currentRateSheetDetails);
+
+        }
+    }
+}
