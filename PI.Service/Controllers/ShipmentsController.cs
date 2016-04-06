e0c6c07e10cd718c37f4d6aa0d2a3b2e905e3316
Harshan1 <hpremarathna@mitrai.com>
2016-04-06 15:20:00 +0530

    Add new file upload code with required libraries.

--- PI.Service/Controllers/ShipmentsController.cs
+++ PI.Service/Controllers/ShipmentsController.cs
@@ -17,8 +17,7 @@ using PI.Common;
 using PI.Contract.Enums;
 using PI.Contract.DTOs.FileUpload;
 using System.IO;
-
-
+using Newtonsoft.Json;
 
 namespace PI.Service.Controllers
 {
@@ -146,52 +145,128 @@ namespace PI.Service.Controllers
         }
 
 
-        [EnableCors(origins: "*", headers: "*", methods: "*")]
-        //[Authorize]
-        [HttpPost]
-        [Route("UploadDocumentsForShipment")]
-        public async Task UploadDocumentsForShipment(FileUploadDto fileUpload)
+        //[EnableCors(origins: "*", headers: "*", methods: "*")]
+        ////[Authorize]
+        //[HttpPost]
+        //[Route("UploadDocumentsForShipment")]
+        //public async Task UploadDocumentsForShipment(FileUploadDto fileUpload)
+        //{
+        //    try
+        //    {
+        //        var provider = GetMultipartProvider();
+        //        var result = await Request.Content.ReadAsMultipartAsync(provider);
+
+
+
+        //        HttpPostedFileBase assignmentFile = fileUpload.Attachment;
+        //        var fileName = fileUpload.Attachment.FileName;
+
+        //        var imageFileNameInFull = string.Format("{0}_{1}", System.Guid.NewGuid().ToString(), fileName);
+
+        //        fileUpload.ClientFileName = fileName;
+        //        fileUpload.UploadedFileName = imageFileNameInFull;
+
+        //        AzureFileManager media = new AzureFileManager();
+        //        media.InitializeStorage(fileUpload.TenantId.ToString(), DocumentType.Shipment.ToString());
+        //        var result1 = await media.Upload(assignmentFile, imageFileNameInFull);
+
+        //        // Insert document record to DB.
+        //        ShipmentsManagement shipmentManagement = new ShipmentsManagement();
+        //        shipmentManagement.InsertShipmentDocument(fileUpload);
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        //throw;
+        //    }
+        //}
+
+        //// You could extract these two private methods to a separate utility class since
+        //// they do not really belong to a controller class but that is up to you
+        //private MultipartFormDataStreamProvider GetMultipartProvider()
+        //{
+        //    // IMPORTANT: replace "(tilde)" with the real tilde character
+        //    // (our editor doesn't allow it, so I just wrote "(tilde)" instead)
+        //    var uploadFolder = "(tilde)/App_Data/Tmp/FileUploads"; // you could put this to web.config
+        //    var root = HttpContext.Current.Server.MapPath(uploadFolder);
+        //    Directory.CreateDirectory(root);
+        //    return new MultipartFormDataStreamProvider(root);
+        //}
+
+        [HttpPost] // This is from System.Web.Http, and not from System.Web.Mvc
+        public async Task<HttpResponseMessage> Upload()
         {
-            try
+            if (!Request.Content.IsMimeMultipartContent())
             {
-                var provider = GetMultipartProvider();
-                var result = await Request.Content.ReadAsMultipartAsync(provider);
-
-
-
-                HttpPostedFileBase assignmentFile = fileUpload.Attachment;
-                var fileName = fileUpload.Attachment.FileName;
-
-                var imageFileNameInFull = string.Format("{0}_{1}", System.Guid.NewGuid().ToString(), fileName);
-
-                fileUpload.ClientFileName = fileName;
-                fileUpload.UploadedFileName = imageFileNameInFull;
-
-                AzureFileManager media = new AzureFileManager();
-                media.InitializeStorage(fileUpload.TenantId.ToString(), DocumentType.Shipment.ToString());
-                var result1 = await media.Upload(assignmentFile, imageFileNameInFull);
-                
-                // Insert document record to DB.
-                ShipmentsManagement shipmentManagement = new ShipmentsManagement();
-                shipmentManagement.InsertShipmentDocument(fileUpload);
-
+                this.Request.CreateResponse(HttpStatusCode.UnsupportedMediaType);
             }
-            catch (Exception ex)
-            {
-                //throw;
-            }
+
+            var provider = GetMultipartProvider();
+            var result = await Request.Content.ReadAsMultipartAsync(provider);
+
+            // On upload, files are given a generic name like "BodyPart_26d6abe1-3ae1-416a-9429-b35f15e6e5d5"
+            // so this is how you can get the original file name
+            var originalFileName = GetDeserializedFileName(result.FileData.First());
+
+            // uploadedFileInfo object will give you some additional stuff like file length,
+            // creation time, directory name, a few filesystem methods etc..
+            var uploadedFileInfo = new FileInfo(result.FileData.First().LocalFileName);
+
+            // Convert to stream
+            
+            MemoryStream data = new MemoryStream();
+            Stream str = File.OpenRead(uploadedFileInfo.FullName);
+            str.CopyTo(data);
+            byte[] buf = new byte[data.Length];
+            data.Read(buf, 0, buf.Length);
+
+            // End of convert to stream
+
+            // Remove this line as well as GetFormData method if you're not
+            // sending any form data with your upload request
+            var fileUploadObj = GetFormData<FileUploadDto>(result);
+
+            // Through the request response you can return an object to the Angular controller
+            // You will be able to access this in the .success callback through its data attribute
+            // If you want to send something to the .error callback, use the HttpStatusCode.BadRequest instead
+            var returnData = "ReturnTest";
+            return this.Request.CreateResponse(HttpStatusCode.OK, new { returnData });
         }
 
         // You could extract these two private methods to a separate utility class since
         // they do not really belong to a controller class but that is up to you
         private MultipartFormDataStreamProvider GetMultipartProvider()
         {
-            // IMPORTANT: replace "(tilde)" with the real tilde character
-            // (our editor doesn't allow it, so I just wrote "(tilde)" instead)
-            var uploadFolder = "(tilde)/App_Data/Tmp/FileUploads"; // you could put this to web.config
+            var uploadFolder = "~/App_Data/Tmp/FileUploads"; // you could put this to web.config
             var root = HttpContext.Current.Server.MapPath(uploadFolder);
             Directory.CreateDirectory(root);
             return new MultipartFormDataStreamProvider(root);
+        }
+
+        // Extracts Request FormatData as a strongly typed model
+        private object GetFormData<T>(MultipartFormDataStreamProvider result)
+        {
+            if (result.FormData.HasKeys())
+            {
+                var unescapedFormData = Uri.UnescapeDataString(result.FormData
+                    .GetValues(0).FirstOrDefault() ?? String.Empty);
+                //if (!String.IsNullOrEmpty(unescapedFormData))
+                //    return JsonConvert.DeserializeObject<T>(unescapedFormData);
+                return unescapedFormData;
+            }
+
+            return null;
+        }
+
+        private string GetDeserializedFileName(MultipartFileData fileData)
+        {
+            var fileName = GetFileName(fileData);
+            return JsonConvert.DeserializeObject(fileName).ToString();
+        }
+
+        public string GetFileName(MultipartFileData fileData)
+        {
+            return fileData.Headers.ContentDisposition.FileName;
         }
 
     }
