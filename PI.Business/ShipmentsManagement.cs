b4e8aac69242e939615bb1c38c72eb3ca8212d09
ShanelPerera <sperera@mitrai.com>
2016-04-07 14:16:42 +0530

    Updated upload method

--- PI.Business/ShipmentsManagement.cs
+++ PI.Business/ShipmentsManagement.cs
@@ -1252,7 +1252,7 @@ namespace PI.Business
         {
             List<FileUploadDto> returnList = new List<FileUploadDto>();
             // Make absolute link
-            string baseUrl = @"https://pidocuments.blob.core.windows.net:443/piblobstorage/";
+            string baseUrl = ConfigurationManager.AppSettings["PIBlobStorage"];
 
             CompanyManagement companyManagement = new CompanyManagement();
             var tenantId = companyManagement.GettenantIdByUserId(userId);
@@ -1417,7 +1417,7 @@ namespace PI.Business
         private string getLabelforShipmentFromBlobStorage(long shipmentId, long tenantId)
         {
             // Make absolute link
-            string baseUrl = @"https://pidocuments.blob.core.windows.net:443/piblobstorage/";
+            string baseUrl = ConfigurationManager.AppSettings["PIBlobStorage"];
 
             string fileAbsoluteURL = baseUrl + "TENANT_" + tenantId + "/" + Utility.GetEnumDescription(DocumentType.ShipmentLabel)
                                                                           + "/" + (shipmentId.ToString() + ".pdf");
--- PI.WebApp/app/shipment/shipmentDocuments.js
+++ PI.WebApp/app/shipment/shipmentDocuments.js
@@ -11,14 +11,16 @@
                        url: serverBaseUrl + '/api/Shipments/upload',
                        data: {
                            file: file,
+                           userId: $window.localStorage.getItem('userGuid'),
+                           documentType: "SHIPMENT_LABEL",
                            referenceId: $scope.overviewShipCtrl.shipment.generalInformation.shipmentId,
-                           userId: $window.localStorage.getItem('userGuid')
                        },
                    });
 
                    file.upload.then(function (response) {
                        $timeout(function () {
                            file.result = response.data;
+                           deleteFile();
                        });
                    }, function (response) {
                        if (response.status > 0)
@@ -27,8 +29,54 @@
                        // Math.min is to fix IE which reports 200% sometimes
                        file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
                    });
-               }
+        }
 
-           }
+
+        
+        akFileUploaderService.saveModel(fileUpload, "/Media/FileUpload")
+            .then(function (data) {
+            debugger;
+            vm.imageFiles.push({
+                fileAbsoluteURL: data[data.length - 1].FileAbsoluteURL,
+                fileNameWithExtention: data[data.length - 1].FileNameWithExtention,
+                categoryId: vm.model.categoryId,
+                id: data[data.length - 1].Id,
+                documentType: vm.model.documentType,
+                contentDescription: vm.model.contentDescription,
+                classification: vm.model.classification,
+                searchAttributes: vm.model.searchAttributes,
+                tag: vm.model.tag,
+                tenantId: abp.session.tenantId
+            });
+            clearUplodFields();
+        });
+
+
+        $scope.deleteFile = function (file) {
+            debugger;
+            $http({
+                url: serverBaseUrl + '/api/Shipments/upload',
+                method: "POST",
+                data: file
+            }).success(function (result) {
+
+                for (var i = 0; i < $scope.fileList.length; i++)
+                    if ($scope.fileList[i].id === file.id) {
+                        $scope.fileList.splice(i, 1);
+                        break;
+                    }
+            }).error(function (result, status) {
+
+            });
+        }
+
+
+        var clearUplodFields = function () {
+
+            $scope.document = null,
+            angular.element("input[name='document']").val(null);
+        }      
+
+      }
         ])
 })(angular.module('newApp'));
