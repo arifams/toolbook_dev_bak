a8439e64134ec31a0589fbbc6f0d86cf08800390
damarasinghe <damarasinghe@mitrai.com>
2016-04-09 10:34:06 +0530

    added new SIS Urls

--- PI.WebApp/app/shipment/ShipmentOverview.js
+++ PI.WebApp/app/shipment/ShipmentOverview.js
@@ -70,8 +70,8 @@
                        shipmentId = vm.shipment.generalInformation.shipmentId;
                        vm.Consigneremail = vm.shipment.addressInformation.consigner.email;
 
-                       vm.awb_URL = "http://www2.shipitsmarter.com/taleus/print_awb.asp?code_shipment=" + vm.shipmentCode + "&email=" + vm.Consigneremail;
-                       vm.cmr_URL = "http://book.parcelinternational.nl/taleus/print_cmr.asp?code_shipment=" + vm.shipmentCode + "&userid=info@parcelinternational.com&password=Shipper01";
+                       vm.awb_URL = SISUrl+ "print_awb.asp?code_shipment=" + vm.shipmentCode + "&email=" + vm.Consigneremail;
+                       vm.cmr_URL = SISUrl2 + "print_cmr.asp?code_shipment=" + vm.shipmentCode + "&userid=" + SISUser + "&password=" + SISPassword;
                        vm.shipmentLabel = data.generalInformation.shipmentLabelBLOBURL;
                        console.log(vm.shipmentLabel);
                        loadShipmentStatuses();
