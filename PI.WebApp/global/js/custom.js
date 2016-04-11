92e16a2d36fa7b03a01c5f66ac35222ba2074fbd
damarasinghe <damarasinghe@mitrai.com>
2016-04-11 15:13:16 +0530

    added URLs

--- PI.WebApp/global/js/custom.js
+++ PI.WebApp/global/js/custom.js
@@ -1,9 +1,17 @@
 //****************** YOUR CUSTOMIZED JAVASCRIPT **********************//
 
 // Service base url
-//var serverBaseUrl = 'http://localhost:5555';
-var serverBaseUrl = 'http://pibooking.azurewebsites.net';
+var serverBaseUrl = 'http://localhost:5555';
+//var serverBaseUrl = 'http://pibooking.azurewebsites.net';
 
 // Web base url
-//var webBaseUrl = 'http://localhost:49995';
-var webBaseUrl = 'http://pibookingservice.azurewebsites.net';
+var webBaseUrl = 'http://localhost:49995';
+//var webBaseUrl = 'http://pibookingservice.azurewebsites.net';
+
+var SISUrl='http://book.12send.com/taleus/';
+
+var SISUrl2='http://book.parcelinternational.nl/taleus/';
+
+var SISUser = 'info@parcelinternational.com';
+
+var SISPassword = 'Shipper01';
