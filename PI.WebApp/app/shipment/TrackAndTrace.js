f97c5beeb577a366bc1a5ed3810a65910c6ed5d8
damarasinghe <damarasinghe@mitrai.com>
2016-04-15 15:24:48 +0530

    added null handler

--- PI.WebApp/app/shipment/TrackAndTrace.js
+++ PI.WebApp/app/shipment/TrackAndTrace.js
@@ -26,41 +26,46 @@
                        vm.missingFields = false;
                        shipmentFactory.getTeackAndTraceDetails(carrier, trackingNo).success(function (data) {
                            vm.trackingData = data;
-                           if (vm.trackingData.history != null) {
-                               vm.trackingNotFound = false;
-                               for (var i = 0; i < vm.trackingData.history.items.length; i++) {
-                                   lat = vm.trackingData.history.items[i].location.geo.lat;
-                                   lng = vm.trackingData.history.items[i].location.geo.lng;
+                           if (vm.trackingData != null) {
 
-                               }
+                               if (vm.trackingData.history != null) {
+                                   vm.trackingNotFound = false;
+                                   for (var i = 0; i < vm.trackingData.history.items.length; i++) {
+                                       lat = vm.trackingData.history.items[i].location.geo.lat;
+                                       lng = vm.trackingData.history.items[i].location.geo.lng;
 
-                               if ($("#simple-map").length) {
-                                   simple_map = new GMaps({
-                                       el: '#simple-map',
-                                       lat: lat,
-                                       lng: lng,
-                                       zoomControl: true,
-                                       zoomControlOpt: {
-                                           style: 'SMALL',
-                                           position: 'TOP_LEFT'
-                                       },
-                                       panControl: false,
-                                       streetViewControl: false,
-                                       mapTypeControl: false,
-                                       overviewMapControl: false
-                                   });
-                                   simple_map.addMarker({
-                                       lat: lat,
-                                       lng: lng,
-                                       title: 'Marker with InfoWindow',
-                                       infoWindow: {
-                                           content: '<p>Here we are!</p>'
-                                       }
-                                   });
+                                   }
+
+                                   if ($("#simple-map").length) {
+                                       simple_map = new GMaps({
+                                           el: '#simple-map',
+                                           lat: lat,
+                                           lng: lng,
+                                           zoomControl: true,
+                                           zoomControlOpt: {
+                                               style: 'SMALL',
+                                               position: 'TOP_LEFT'
+                                           },
+                                           panControl: false,
+                                           streetViewControl: false,
+                                           mapTypeControl: false,
+                                           overviewMapControl: false
+                                       });
+                                       simple_map.addMarker({
+                                           lat: lat,
+                                           lng: lng,
+                                           title: 'Marker with InfoWindow',
+                                           infoWindow: {
+                                               content: '<p>Here we are!</p>'
+                                           }
+                                       });
+                                   }
+                               } else {
+                                   vm.trackingNotFound = true;
                                }
                            } else {
                                vm.trackingNotFound = true;
-                           }
+                           }                          
 
 
                        })
