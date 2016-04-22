3de24a6cdf63e4733b4c35014e2b08c1415edca1
damarasinghe <damarasinghe@mitrai.com>
2016-04-22 15:22:34 +0530

    added print function in AWB

--- PI.WebApp/app/shipment/ShipmentOverview.js
+++ PI.WebApp/app/shipment/ShipmentOverview.js
@@ -82,6 +82,14 @@
                    })
                }
 
+               //
+               vm.printAwb = function (divId) {
+                   var printContents = document.getElementById(divId).innerHTML;
+                   var popupWin = window.open('', '_blank', 'width=800,height=800');
+                   popupWin.document.open();
+                   popupWin.document.write('<html><head></head><body onload="window.print()">' + printContents + '</body></html>');
+                   popupWin.document.close();
+               }
 
                //get the current shipment details
                var loadShipmentInfo = function () {
@@ -99,7 +107,7 @@
                        vm.cmr_URL = SISUrl2 + "print_cmr.asp?code_shipment=" + vm.shipmentCode + "&userid=" + SISUser + "&password=" + SISPassword;
                        vm.shipmentLabel = data.generalInformation.shipmentLabelBLOBURL;
 
-                       $('<iframe src="' + vm.awb_URL + '" frameborder="0" scrolling="no" id="myFrame" height="900" width="700"></iframe>').appendTo('.awb');
+                       $('<iframe src="' + vm.awb_URL + '" frameborder="0" scrolling="no" id="myFrame" height="867" width="700"></iframe>').appendTo('.awb');
                        $('<iframe src="' + vm.cmr_URL + '" frameborder="0" scrolling="no" id="myFrame" height="900" width="800"></iframe>').appendTo('.cmr');
                      //  console.log(vm.shipmentLabel);
                        loadShipmentStatuses();
