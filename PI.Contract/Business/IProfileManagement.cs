88feaee02c00d4e687d3508205d6d8539085f403
damarasinghe <damarasinghe@mitrai.com>
2016-05-03 20:31:36 +0530

    added untracked files

---
+++ PI.Contract/Business/IProfileManagement.cs
@@ -0,0 +1,23 @@
+﻿using PI.Contract.DTOs.Profile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI.Contract.Business
+{
+    public interface IProfileManagement
+    {
+        ProfileDto getProfileByUserName(string username);
+        ProfileDto GetAccountSettings(long customerId);
+        ProfileDto GetCustomerAddressDetails(long cusomerAddressId, long companyId);
+        ProfileDto getProfileByUserNameForShipment(string username);
+        int UpdateProfileAccountSettings(ProfileDto updatedProfile);
+        int UpdateProfileAddress(ProfileDto updatedProfile);
+        int UpdateProfileBillingAddress(ProfileDto updatedProfile);
+        int UpdateProfileLoginDetails(ProfileDto updatedProfile);
+        int UpdateProfileGeneral(ProfileDto updatedProfile);
+        int updateProfileData(ProfileDto updatedProfile);
+    }
+}
