6da6c3b067b44001e0f06ad2097112d419221d43
damarasinghe <damarasinghe@mitrai.com>
2016-05-09 10:03:16 +0530

    added search company popup

---
+++ PI.WebApp/app/shipment/CompanyViewTemplate.js
@@ -0,0 +1,23 @@
+﻿
+'use strict';
+
+
+(function (app) {
+
+    app.controller('companyListCtrl',
+       ['$location', '$window', 'shipmentFactory', '$scope', 'searchList',
+    function ($location, $window, shipmentFactory, $scope, searchList) {
+
+        $scope.companyCollection = searchList;
+
+
+        //set selected address details
+        $scope.selectCompany = function (address) {
+   
+
+        }
+
+    }]);
+
+
+})(angular.module('newApp'));
--- PI.WebApp/app/shipment/shipmentManagement.js
+++ PI.WebApp/app/shipment/shipmentManagement.js
@@ -1,23 +1,44 @@
 ﻿'use strict';
 (function (app) {
 
-    app.controller('shipmentManageCtrl', ['$scope', '$location', '$window', 'shipmentFactory',
-                       function ($scope, $location, $window, shipmentFactory) {
+    app.controller('shipmentManageCtrl', ['$scope', '$location', '$window', 'shipmentFactory','ngDialog','$controller',
+                       function ($scope, $location, $window, shipmentFactory, ngDialog, $controller) {
 
                            var vm = this;
                            vm.searchText = '';                          
+                     
+                           vm.loadAllCompanies = function () {
 
-                           vm.loadAllCompanies = function () {                             
-                               
-                              var searchtext= vm.searchText;
-                               shipmentFactory.loadAllcompanies(searchtext)
-                                   .success(                                   
-                                          function (responce) {
-                                             // vm.rowCollection = responce.content;
-                                          }).error(function (error) {
-                                              console.log("error occurd while retrieving shiments");
+                               shipmentFactory.loadAllcompanies(vm.searchText).success(
+                                  function (responce) {
+                                      if (responce.content.length > 0) {
+
+                                          ngDialog.open({
+                                              scope: $scope,
+                                              template: '/app/shipment/CompanyViewTemplate.html',
+                                              className: 'ngdialog-theme-default',
+                                              controller: $controller('companyListCtrl', {
+                                                  $scope: $scope,
+                                                  searchList: responce.content                                             
+                                              })
+
                                           });
+
+
+                                      } else {
+                                          vm.addressDetailsEmpty = true;
+                                          vm.emptySearch = false;
+                                      }
+                                  }).error(function (error) {
+
+                                      console.log("error occurd while retrieving Addresses");
+                                  });
+
                            }
+
+
+
+
        
                        }])
 })(angular.module('newApp'));
