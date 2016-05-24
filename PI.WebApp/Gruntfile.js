ca34a0b995f8cd45e4a104a04c1b08c17650cfa2
ShanelPerera <sperera@mitrai.com>
2016-05-24 14:42:17 +0530

    Added code changes for localization

---
+++ PI.WebApp/Gruntfile.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        nggettext_extract: {
+            pot: {
+                files: {
+                    'po/template.pot': 'app/**/*.html'//['*.html']
+                }
+            },
+        },
+
+        nggettext_compile: {
+            all: {
+                files: {
+                    'js/translations.js': ['po/*.po']
+                }
+            },
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-angular-gettext');
+
+    grunt.registerTask('default', ['nggettext_extract', 'nggettext_compile']);
+};
---
+++ PI.WebApp/js/translations.js
@@ -0,0 +1,5 @@
+angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
+/* jshint -W100 */
+    gettextCatalog.setStrings('nl', {"Account Type":"Gebruikersaccount","Enter at least 3 characters or more":"Voer ten minste 3 tekens of meer","Purchase order or description of the shipment":"Inkooporder of beschrijving van de zending"});
+/* jshint +W100 */
+}]);
