d50169f87f532e86ad15c29c2b0afbcab7c5e8e1
ShanelPerera <sperera@mitrai.com>
2016-06-09 14:47:19 +0530

    Added changes to dashboard

--- PI.WebApp/app/dashboard/dashboardCtrl.js
+++ PI.WebApp/app/dashboard/dashboardCtrl.js
@@ -1,25 +1,37 @@
-﻿
-'use strict';
-
+﻿'use strict';
 
 (function (app) {
 
-    app.controller('dashboardCtrl',
-       ['$scope', 'builderFactory',
-           function ($scope, builderFactory) {
+    app.factory('dashboardfactory', function ($http) {
+        return {
+            getShipmentStatusCounts: function (status) {
+                return $http.get(serverBaseUrl + '/api/shipments/GetShipmentStatusCounts', {
+                    params: {
+                        status: status,
+                        userId: $window.localStorage.getItem('userGuid')
+                    }
+                })
+            }
+        });
 
-              $scope.$on('$viewContentLoaded', function () {
-                  builderFactory.loadLineChart();
-                  builderFactory.loadDougnutChart1();
-                  builderFactory.loadDougnutChart2();
-                  builderFactory.loadDougnutChart3();
-                  builderFactory.loadDougnutChart4();
-              });
-
-              this.isViaDashboard = true;
+        app.controller('dashboardCtrl',
+           ['$scope', 'builderFactory',
+               function ($scope, builderFactory) {
 
 
-           }]);
+
+                   $scope.$on('$viewContentLoaded', function () {
+                       builderFactory.loadLineChart();
+                       builderFactory.loadDougnutChart1();
+                       builderFactory.loadDougnutChart2();
+                       builderFactory.loadDougnutChart3();
+                       builderFactory.loadDougnutChart4();
+                   });
+
+                   this.isViaDashboard = true;
 
 
-})(angular.module('newApp'));
+               }]);
+
+
+    })(angular.module('newApp'));
