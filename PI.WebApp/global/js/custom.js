772d159227c2642eb4717b45cc73141b2e31c8d8
damarasinghe <damarasinghe@mitrai.com>
2016-06-24 18:59:12 +0530

    added QA Urls

--- PI.WebApp/global/js/custom.js
+++ PI.WebApp/global/js/custom.js
@@ -4,24 +4,24 @@
 //var serverBaseUrl = 'http://localhost:5555';
 //var serverBaseUrl = 'http://pibooking.azurewebsites.net';
 //var serverBaseUrl = 'http://piserviceprod.azurewebsites.net';
-//var serverBaseUrl = 'http://piserviceqa.azurewebsites.net';
-var serverBaseUrl = 'http://piserviceuat.azurewebsites.net';
+var serverBaseUrl = 'http://piserviceqa.azurewebsites.net';
+//var serverBaseUrl = 'http://piserviceuat.azurewebsites.net';
 
 
 // Web base url
 //var webBaseUrl = 'http://localhost:49995';
 //var webBaseUrl = 'http://pibookingservice.azurewebsites.net';
 //var webBaseUrl = 'http://www.parcelinternational.net';
-//var webBaseUrl = 'http://piwebqa.azurewebsites.net';
-var webBaseUrl = 'http://piwebuat.azurewebsites.net';
+var webBaseUrl = 'http://piwebqa.azurewebsites.net';
+//var webBaseUrl = 'http://piwebuat.azurewebsites.net';
 //var webBaseUrl = 'http://book.parcelinternational.com';
 
 
-//var SISUrlUS = 'http://www2.shipitsmarter.com/taleus/';
-//var SISUrlNL = 'http://www2.shipitsmarter.com/tale/';
+var SISUrlUS = 'http://www2.shipitsmarter.com/taleus/';
+var SISUrlNL = 'http://www2.shipitsmarter.com/tale/';
 
-var SISUrlUS = 'http://book.parcelinternational.nl/taleus/';
-var SISUrlNL = 'http://book.parcelinternational.nl/tale/';
+//var SISUrlUS = 'http://book.parcelinternational.nl/taleus/';
+//var SISUrlNL = 'http://book.parcelinternational.nl/tale/';
 
 var SISUser = 'info@parcelinternational.com';
 
