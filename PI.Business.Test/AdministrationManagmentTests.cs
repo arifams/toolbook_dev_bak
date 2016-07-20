674308664875fd82ddc6a347d520d6b02299095e
Harshan1 <hpremarathna@mitrai.com>
2016-07-20 09:47:55 +0530

    Added unit testing classes.

---
+++ PI.Business.Test/AdministrationManagmentTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PI.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI.Business.Tests
+{
+    [TestClass()]
+    public class AdministrationManagmentTests
+    {
+        [TestMethod()]
+        public void ImportRateSheetExcelTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod()]
+        public void ManageInvoicePaymentSettingTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod()]
+        public void GetAuditTrailsForCustomerTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
