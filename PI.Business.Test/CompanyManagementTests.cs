e9d3f8ef6681fa0573a9995b455ab8c7cc7a3273
damarasinghe <damarasinghe@mitrai.com>
2016-07-27 15:30:32 +0530

    PAR-390 done

--- PI.Business.Test/CompanyManagementTests.cs
+++ PI.Business.Test/CompanyManagementTests.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PI.Business;
+using PI.Contract.DTOs.Address;
+using PI.Contract.DTOs.Customer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,16 +13,60 @@ namespace PI.Business.Tests
     [TestClass()]
     public class CompanyManagementTests
     {
+        CompanyManagement company = null;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            company = new CompanyManagement();
+        }
+
         [TestMethod()]
         public void CreateCompanyDetailsTest()
         {
-            
+           
+            CustomerDto dto = new CustomerDto()
+            {
+                AddressId = 1,
+                Code = "1234",
+                CompanyCode = "Comp1234",
+                CompanyId = 10,
+                CompanyName = "Test Corp",
+                Email = "test1234@yopmail.com",
+                FirstName = "FName",
+                LastName = "LName",
+                IsConfirmEmail = true,
+                IsCorporateAccount = true,
+                MobileNumber = "1231231233",
+                PhoneNumber = "1231231233",
+                Password = "TestPassword",
+                UserId = "1",
+                Salutation = "Mr",
+                Id =20 ,
+                CustomerAddress= new AddressDto
+                {
+                    Id=100,
+                    StreetAddress1="custAdd1",
+                    StreetAddress2="custAdd2",
+                    City="CityTest",
+                    Country="US",
+                    Number="2222",
+                    State="California",
+                    ZipCode="12314"
+                    
+                }             
+            };
+
+            var response = company.CreateCompanyDetails(dto);
+            Assert.AreNotEqual(response, 0);
         }
 
         [TestMethod()]
         public void GetAllCostCentersForCompanyTest()
         {
-            
+            //string userId=""
+            //var response = company.GetAllCostCentersForCompany();
+            //Assert.AreNotEqual(response, null);
         }
 
         [TestMethod()]
