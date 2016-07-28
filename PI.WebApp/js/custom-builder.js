1c8dbc9021f566a24cd3ebcffb1873b748a1cbbf
Harshan1 <hpremarathna@mitrai.com>
2016-07-28 14:48:02 +0530

    Load user based on the role.

--- PI.WebApp/js/custom-builder.js
+++ PI.WebApp/js/custom-builder.js
@@ -72,11 +72,37 @@
        
         orgStructurePopup: function (datascource) {
             
+            var secondMenu = '';
+
             $('#chart-container').orgchart({
                 'data': datascource,
                 'nodeContent': 'title',
                 'nodeID': 'id',
                 'createNode': function ($node, data) {
+
+                    if (data.type == "businessowner") {
+                        console.log('businessowner');
+                        secondMenu = '<div class="second-menu"><div dropdown="" class="btn-group"><button data-toggle="dropdown" class="btn btn-default dropdown-toggle" type="button" aria-haspopup="true" data-toggle="dropdown"><span class="caret"></span></button><span class="dropdown-arrow"></span><ul role="menu" class="dropdown-menu">'
+                        + '<li><a href="javascript:;" ng-click="loadUserManagment(0,\'Manager\')">Manager</a></li><li><a href="javascript:;" ng-click="loadUserManagment(0,\'Supervisor\')">Supervisor</a></li><li><a href="javascript:;" ng-click="loadUserManagment(0,\'Operator\')">Operator</a></li><li><a href="javascript:;" ng-click="loadDivisionManagment(0)">Division</a></li>'
+                        + '</ul></div></div>';
+                    }
+                    else if (data.type == "manager"){
+                        console.log('manager');
+                        secondMenu = '<div class="second-menu"><div dropdown="" class="btn-group"><button data-toggle="dropdown" class="btn btn-default dropdown-toggle" type="button" aria-haspopup="true" data-toggle="dropdown"><span class="caret"></span></button><span class="dropdown-arrow"></span><ul role="menu" class="dropdown-menu">'
+                        + '<li><a href="javascript:;" ng-click="loadUserManagment(0,\'Supervisor\')">Supervisor</a></li><li><a href="javascript:;" ng-click="loadUserManagment(0,\'Operator\')">Operator</a></li><li><a href="javascript:;" ng-click="loadDivisionManagment(0)">Division</a></li>'
+                        + '</ul></div></div>';
+                    }
+                    else if (data.type == "supervisor") {
+                        console.log('supervisor');
+                        secondMenu = '<div class="second-menu"><div dropdown="" class="btn-group"><button data-toggle="dropdown" class="btn btn-default dropdown-toggle" type="button" aria-haspopup="true" data-toggle="dropdown"><span class="caret"></span></button><span class="dropdown-arrow"></span><ul role="menu" class="dropdown-menu">'
+                        + '<li><a href="javascript:;" ng-click="loadDivisionManagment(0)">Division</a></li></ul></div></div>';
+                    }
+                    else if (data.type == "division") {
+                        console.log('division');
+                        secondMenu = '<div class="second-menu"><div dropdown="" class="btn-group"><button data-toggle="dropdown" class="btn btn-default dropdown-toggle" type="button" aria-haspopup="true" data-toggle="dropdown"><span class="caret"></span></button><span class="dropdown-arrow"></span><ul role="menu" class="dropdown-menu">'
+                        + '<li><a href="javascript:;" ng-click="loadUserManagment(0,\'Operator\')">Operator</a></li><li><a href="javascript:;" ng-click="loadCostcenterManagement(0)">Cost Center</a></li></ul></div></div>';
+                    }
+
                     var secondMenuIcon = $('<i>', {
                         'class': 'fa fa-plus-circle second-menu-icon',
                         click: function () {
@@ -84,7 +110,8 @@
 
                         }
                     });
-                    var secondMenu = '<div class="second-menu"><div dropdown="" class="btn-group"><button data-toggle="dropdown" class="btn btn-default dropdown-toggle" type="button" aria-haspopup="true" data-toggle="dropdown"><span class="caret"></span></button><span class="dropdown-arrow"></span><ul role="menu" class="dropdown-menu"><li><a href="javascript:;" ng-click="loadUserManagment(0)">add user1</a></li><li><a href="javascript:;" ng-click="loadDivisionManagment(0)">add user2</a></li><li><a href="javascript:;" ng-click="loadCostcenterManagement(0)">add user3</a></li></ul></div></div>';
+
+                   // var secondMenu = '<div class="second-menu"><div dropdown="" class="btn-group"><button data-toggle="dropdown" class="btn btn-default dropdown-toggle" type="button" aria-haspopup="true" data-toggle="dropdown"><span class="caret"></span></button><span class="dropdown-arrow"></span><ul role="menu" class="dropdown-menu"><li><a href="javascript:;" ng-click="loadUserManagment(0)">add user1</a></li><li><a href="javascript:;" ng-click="loadDivisionManagment(0)">add user2</a></li><li><a href="javascript:;" ng-click="loadCostcenterManagement(0)">add user3</a></li></ul></div></div>';
                     $node.append(secondMenuIcon).append(secondMenu);
                 }
             });
