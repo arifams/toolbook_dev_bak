c38ac07bdf5cd3f96ac7d4de9d2c66cdc4c1f40b
damarasinghe <damarasinghe@mitrai.com>
2016-08-01 11:21:33 +0530

    added test methods for profile management

--- PI.Business.Test/ProfileManagementTests.cs
+++ PI.Business.Test/ProfileManagementTests.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PI.Business;
+using PI.Contract.DTOs.AccountSettings;
 using PI.Contract.DTOs.Address;
 using PI.Contract.DTOs.Company;
 using PI.Contract.DTOs.CostCenter;
 using PI.Contract.DTOs.Customer;
 using PI.Contract.DTOs.Profile;
+using PI.Data.Entity;
+using PI.Data.Entity.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -136,127 +139,226 @@ namespace PI.Business.Tests
         [TestMethod()]
         public void UpdateSetupWizardBillingAddressTest()
         {
-            new NotImplementedException();
-        }
+            ProfileDto profileDto = new ProfileDto()
+            {
+                CustomerDetails = new CustomerDto()
+                {
+                    UserId = "",
+                    UserName = "UserName",
+                    Salutation = "Mr",
+                    FirstName = "FirstName",
+                    MiddleName = "MiddleName",
+                    LastName = "LastName",
+                    Email = "Email@E.com",
+                    JobCapacity = "JobCapacity",
+                    IsCorporateAccount = true
+                }
+            };
+
+            int response = profile.UpdateSetupWizardBillingAddress(profileDto);
+            Assert.AreEqual(response, 1)
+;        }
 
         [TestMethod()]
         public void UpdateProfileLoginDetailsTest()
         {
-            new NotImplementedException();
+            ProfileDto profileDto = new ProfileDto()
+            {
+                NewPassword= "NewPassword",
+                CustomerDetails = new CustomerDto()
+                {
+                    UserId = "",
+                    UserName = "UserName"                    
+                }              
+
+            };
+
+            int response = profile.UpdateProfileLoginDetails(profileDto);
+            Assert.AreEqual(response, 1);
         }
 
         [TestMethod()]
         public void UpdateProfileAccountSettingsTest()
         {
-            
+            ProfileDto profileDto = new ProfileDto()
+            {
+                NewPassword = "NewPassword",
+                DefaultLanguageId=1,
+                DefaultCurrencyId=1,
+                DefaultTimeZoneId=1,
+                DefaultWeightMetricId=1,
+                DefaultVolumeMetricId=1,
+
+                BookingConfirmation=true,
+                PickupConfirmation=true,
+                ShipmentDelay=true,
+                ShipmentException=true,
+                NotifyNewSolution=true,
+                NotifyDiscountOffer=true,
+
+                CustomerDetails = new CustomerDto()
+                {
+                    UserId = "",
+                    UserName = "UserName"
+                }
+
+            };
+
+            int response = profile.UpdateProfileAccountSettings(profileDto);
+            Assert.AreEqual(response, 1);
         }
 
         [TestMethod()]
         public void UpdateThemeColourTest()
         {
-            
+            ProfileDto profileDto = new ProfileDto()
+            {
+                SelectedColour= "#396485",
+                CustomerDetails = new CustomerDto()
+                {
+                    UserId = "",
+                    UserName = "UserName"
+                }
+
+            };
+            int response = profile.UpdateThemeColour(profileDto);
+
         }
 
         [TestMethod()]
         public void GetUserbyUserNameTest()
         {
-            new NotImplementedException();
+            string UserName = "info@parcelinternational.com";
+            ApplicationUser response = profile.GetUserbyUserName(UserName);
+            Assert.AreNotEqual(response, null);
         }
 
         [TestMethod()]
         public void GetCustomerByUserIdTest()
         {
-            
+            string userId = "";
+            Customer response = profile.GetCustomerByUserId(userId);
+            Assert.AreNotEqual(response, null);
         }
 
         [TestMethod()]
         public void GetLanguageCodeByUserIdTest()
         {
-            new NotImplementedException();
+            string userId = "";
+            string response=profile.GetLanguageCodeByUserId(userId);
+            Assert.AreNotEqual(response, null);
         }
 
         [TestMethod()]
         public void GetCustomerByUserEmailTest()
         {
-            new NotImplementedException();
+            string username = "";
+            Customer response = profile.GetCustomerByUserEmail(username);
+            Assert.AreNotEqual(response, null);
         }
 
         [TestMethod()]
         public void GetUserByIdTest()
         {
-            new NotImplementedException();
+            string userId = "";
+            ApplicationUser response = profile.GetUserById(userId);
+            Assert.AreNotEqual(response, null);
         }
 
         [TestMethod()]
         public void GetAddressbyIdTest()
         {
-            
+            long addressId = 1;
+            Address response = profile.GetAddressbyId(addressId);
+            Assert.AreNotEqual(response, null);
         }
 
         [TestMethod()]
         public void GetAccountSettingByCustomerIdTest()
         {
-            
+            long customerId = 1;
+            AccountSettings response = profile.GetAccountSettingByCustomerId(customerId);
+            Assert.AreNotEqual(response, 1);
         }
 
         [TestMethod()]
         public void GetNotificationCriteriaByCustomerIdTest()
         {
-            
+            long customerId = 1;
+            NotificationCriteria response = profile.GetNotificationCriteriaByCustomerId(customerId);
+            Assert.AreNotEqual(response, null);
         }
 
         [TestMethod()]
         public void GetCompanyByTenantIdTest()
         {
-            
+            long TenantId = 1;
+            Company response = profile.GetCompanyByTenantId(TenantId);
+            Assert.AreNotEqual(response, null);
         }
 
         [TestMethod()]
         public void GetTenantByIdTest()
         {
+            long TenantId = 1;
+            Tenant response = profile.GetTenantById(TenantId);
+            Assert.AreNotEqual(response, null);
             
         }
 
         [TestMethod()]
         public void GetCostCenterByCompanyIdTest()
         {
-            
+            long companyId = 1;
+            IQueryable<CostCenter> response = profile.GetCostCenterByCompanyId(companyId);
+            Assert.AreNotEqual(response.Count(), 0);
         }
 
         [TestMethod()]
         public void GetCostCenterByIdTest()
         {
-            
+            long CostCenterId = 1;
+            CostCenter response = profile.GetCostCenterById(CostCenterId);
+            Assert.AreNotEqual(response, null);
         }
 
         [TestMethod()]
         public void GetCustomerAddressDetailsTest()
         {
-            
+            long cusomerAddressId=1;
+            long companyId = 1;
+            ProfileDto response = profile.GetCustomerAddressDetails(cusomerAddressId, companyId);
+            Assert.AreNotEqual(response, null);
         }
 
         [TestMethod()]
         public void GetAccountSettingsTest()
         {
-            
+            long customerId = 1;
+            ProfileDto response = profile.GetAccountSettings(customerId);
+            Assert.AreNotEqual(response, null);
         }
 
         [TestMethod()]
         public void GetRoleNameByIdTest()
         {
-            
+            string id = "";
+            string response = profile.GetRoleNameById(id);
+            Assert.AreNotEqual(response, string.Empty);
         }
 
         [TestMethod()]
         public void GetAllCurrenciesTest()
         {
-            
+            List<CurrencyDto> response = profile.GetAllCurrencies();
+            Assert.AreNotEqual(response, null);
         }
 
         [TestMethod()]
         public void GetAllTimeZonesTest()
         {
-            
+            List<TimeZoneDto> response = profile.GetAllTimeZones();
+            Assert.AreNotEqual(response, null);
         }
     }
 }
