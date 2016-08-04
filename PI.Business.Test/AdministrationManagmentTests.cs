c8033a064544e9b63f7bf3f8b6af50acbd57574a
damarasinghe <damarasinghe@mitrai.com>
2016-08-04 10:19:09 +0530

    added unit tests for admin management

--- PI.Business.Test/AdministrationManagmentTests.cs
+++ PI.Business.Test/AdministrationManagmentTests.cs
@@ -1,5 +1,8 @@
 ﻿using NUnit.Framework;
 using PI.Business;
+using PI.Contract.DTOs;
+using PI.Contract.DTOs.AuditTrail;
+using PI.Contract.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,22 +14,35 @@ namespace PI.Business.Tests
     [TestFixture]
     public class AdministrationManagmentTests
     {
+        AdministrationManagment admin = null;
+
+        public AdministrationManagmentTests()
+        {
+            admin = new AdministrationManagment();
+        }
+
         [Test]
         public void ImportRateSheetExcelTest()
         {
-            
+            string URI = "";
+            OperationResult response = admin.ImportRateSheetExcel(URI);
+            Assert.AreEqual(response.Status, Status.Success);
         }
 
         [Test]
         public void ManageInvoicePaymentSettingTest()
         {
-            
+            long comapnyId = 1;
+            bool response = admin.ManageInvoicePaymentSetting(comapnyId);
+            Assert.AreEqual(response, true);
         }
 
         [Test]
         public void GetAuditTrailsForCustomerTest()
         {
-            
+            string userId = "";
+            List<AuditTrailDto> response = admin.GetAuditTrailsForCustomer(userId);
+            Assert.AreNotEqual(response.Count, 0);
         }
     }
 }
