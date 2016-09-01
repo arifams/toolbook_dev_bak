4f287fa8439963bd3ce31718d8585a5ba2cb322e
Harshan1 <hpremarathna@mitrai.com>
2016-09-01 13:02:28 +0530

    Fixed build error

--- PI.Service/Controllers/AccountsController.cs
+++ PI.Service/Controllers/AccountsController.cs
@@ -21,7 +21,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Cors;
-using WebApplication3.Results;
+using PI.Service.Results;
 
 namespace PI.Service.Controllers
 {
