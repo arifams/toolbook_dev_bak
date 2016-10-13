2b33edc8818355cdc6aeafd2a38921c730bfb7ed
tgodakumbura <tgodakumbura@mitrai.net>
2016-10-13 10:35:58 +0530

    added bug fixes

---
+++ PI.WebApp/app/admin/manageCustomers.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+(function (app) {
+    app.controller('adminManageCustomersCtrl', ['customBuilderFactory', function (customBuilderFactory) {
+
+        var vm = this;
+        //toggle function
+        vm.loadFilterToggle = function () {
+            customBuilderFactory.customFilterToggle();
+
+        };
+        
+    }]);
+
+})(angular.module('newApp'));
