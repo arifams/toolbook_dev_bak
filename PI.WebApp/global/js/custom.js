7765e97b16995f836f57c431f6c2777c9a91d4e4
ShanelPerera <sperera@mitrai.com>
2016-11-23 14:15:02 +0530

    Revert "updated"
    This reverts commit 5ec80ab2027fd498c0b3fc5633dd5392f6b15e59.

--- PI.WebApp/global/js/custom.js
+++ PI.WebApp/global/js/custom.js
@@ -1,25 +1,25 @@
 //****************** YOUR CUSTOMIZED JAVASCRIPT **********************//
 
 // Service base url
-var serverBaseUrl = 'https://localhost:44339';
+//var serverBaseUrl = 'https://localhost:44339';
 //var serverBaseUrl = 'http://pibooking.azurewebsites.net';
 //var serverBaseUrl = 'http://piserviceprod.azurewebsites.net';
 //var serverBaseUrl = 'http://piserviceqa.azurewebsites.net'; // This is not using anymore
 //var serverBaseUrl = 'https://service.transportal.it';
 //var serverBaseUrl = 'http://piserviceuat.azurewebsites.net';
 
-////var serverBaseUrl = 'https://transportal-qa-service.azurewebsites.net';
+var serverBaseUrl = 'https://transportal-qa-service.azurewebsites.net';
 //var serverBaseUrl = 'http://transportal-prod-service.azurewebsites.net';
 
 
 // Web base url
-var webBaseUrl = 'http://localhost:49995';
+//var webBaseUrl = 'http://localhost:49995';
 //var webBaseUrl = 'http://pibookingservice.azurewebsites.net';
 //var webBaseUrl = 'http://www.parcelinternational.net';
 //var webBaseUrl = 'https://web.transportal.it';
 //var webBaseUrl = 'http://piwebuat.azurewebsites.net';
 
-//var webBaseUrl = 'https://transportal-qa-web.azurewebsites.net';
+var webBaseUrl = 'https://transportal-qa-web.azurewebsites.net';
 //var webBaseUrl = 'http://transportal-prod-web.azurewebsites.net';
 
 var SISUrlUS = 'http://www2.shipitsmarter.com/taleus/';
