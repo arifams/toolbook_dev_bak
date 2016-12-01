6a1abd6681e2e90c0848e49b5e83376cef62b986
dilshanamarasinghe1 <damarasinghe@mitrai.com>
2016-12-01 11:02:20 +0530

    added USP curier name

--- PI.Business/ShipmentsManagement.cs
+++ PI.Business/ShipmentsManagement.cs
@@ -94,7 +94,7 @@ namespace PI.Business
                 // currentRateSheetDetails.
                 if (currentShipment.GeneralInformation.ShipmentMode == "Express")
                 {
-                    currentRateSheetDetails.courier = "UPSDHLFEDTNT";
+                    currentRateSheetDetails.courier = "UPSDHLFEDTNTUSP";
                 }
                 else if (currentShipment.GeneralInformation.ShipmentMode == "AirFreight")
                 {
@@ -111,7 +111,7 @@ namespace PI.Business
                 else
                 {
                     //select all shipment modes
-                    currentRateSheetDetails.courier = "UPSDHLFEDTNT";
+                    currentRateSheetDetails.courier = "UPSDHLFEDTNTUSP";
                     currentRateSheetDetails.courier_air = "EME";
                     currentRateSheetDetails.courier_sea = "EME";
                     currentRateSheetDetails.courier_road = "EME";
@@ -320,7 +320,7 @@ namespace PI.Business
             // currentRateSheetDetails.courier_tariff_type = "NLPARUPS:NLPARFED:USPARDHL2:USPARTNT:USPARUPS:USPARFED2:USUPSTNT:USPAREME:USPARPAE:NLPARTNT2:NLPARDPD:USPARUSP";
             // currentRateSheetDetails.courier_tariff_type = "NLPARUPS:NLPARFED:USPARDHL2:USPARTNT:USPARUPS:USPARFED2:USUPSTNT:USPAREME:USPARPAE:NLPARTNT2:NLPARDPD";
             currentRateSheetDetails.courier_tariff_type = "NLPARUPS:NLPARFED:USPARDHL2:USPARTNT:USPARUPS:USPARFED2:NLPARTNT2:NLPARDPD:USPARUSP";
-
+          //  currentRateSheetDetails.courier_tariff_type = "USUSPS";
             // currentRateSheetDetails.date_pickup = "10-Mar-2016 00:00";//preferredCollectionDate
             // currentRateSheetDetails.time_pickup = "12:51";
             // currentRateSheetDetails.date_delivery_request = "25-Mar-2016 00:00";
