0f2909a41c6f95192a5466aa84d118a39414a39e
Harshan1 <hpremarathna@mitrai.com>
2016-12-28 18:16:40 +0530

    Fixed issue of password validation on Customer create on Admin panel. Fixed issue of deleting customer, if account inactive.

--- PI.WebApp/app/admin/manageCustomers.js
+++ PI.WebApp/app/admin/manageCustomers.js
@@ -1,6 +1,42 @@
 ﻿'use strict';
 
 (function (app) {
+
+    app.directive('customerValidPasswordC', function () {
+        return {
+            require: 'ngModel',
+            link: function (scope, elm, attrs, ctrl) {
+                ctrl.$parsers.unshift(function (viewValue, $scope) {
+                    var noMatch = viewValue != scope.formSaveCustomer.password.$viewValue;
+                    ctrl.$setValidity('noMatch', !noMatch);
+                    return viewValue;
+                })
+            }
+        }
+    });
+
+    app.directive('customerValidPassword', function () {
+        return {
+            require: 'ngModel',
+            link: function (scope, elm, attrs, ctrl) {
+                ctrl.$parsers.unshift(function (viewValue, $scope) {
+
+                    // password validate.
+                    var res = /^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\S]{7,20}$/.test(viewValue);
+                    ctrl.$setValidity('noValidPassword', res);
+
+                    // if change the password when having confirmation password, check match and give error.
+                    if (scope.formSaveCustomer.passwordConf.$viewValue != '') {
+                        var noMatch = viewValue != scope.formSaveCustomer.passwordConf.$viewValue;
+                        scope.formSaveCustomer.passwordConf.$setValidity('noMatch', !noMatch);
+                    }
+
+                    return viewValue;
+                })
+            }
+        }
+    });
+
     app.controller('adminManageCustomersCtrl', ['customBuilderFactory',
         function (customBuilderFactory) {
 
