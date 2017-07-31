dc7ae1bf330e99d0966baee5fc36b75525b11ea7
arifpackage <akurniawan@parcelinternational.com>
2017-07-31 15:11:48 +0200

    arif setting environment - begin

--- PI.WebApp/global/js/custom.js
+++ PI.WebApp/global/js/custom.js
@@ -1,24 +1,24 @@
 //****************** YOUR CUSTOMIZED JAVASCRIPT **********************//
 
 // Service base url
-//var serverBaseUrl = 'https://localhost:44339';
+ var serverBaseUrl = 'https://localhost:44339'; 
 //var serverBaseUrl = 'http://pibooking.azurewebsites.net';
 //var serverBaseUrl = 'http://piserviceprod.azurewebsites.net';
 //var serverBaseUrl = 'http://piserviceqa.azurewebsites.net'; // This is not using anymore
 //var serverBaseUrl = 'https://service.transportal.it';
 //var serverBaseUrl = 'http://piserviceuat.azurewebsites.net';
 
-var serverBaseUrl = 'https://transportal-qa-service.azurewebsites.net';
+// var serverBaseUrl = 'https://transportal-qa-service.azurewebsites.net';
 //var serverBaseUrl = 'https://transportal-prod-service.azurewebsites.net';
 
 
 // Web base url
-//var webBaseUrl = 'http://localhost:49995';
+ var webBaseUrl = 'http://localhost:49995';
 //var webBaseUrl = 'http://pibookingservice.azurewebsites.net';
 //var webBaseUrl = 'http://www.parcelinternational.net';
 //var webBaseUrl = 'https://web.transportal.it';
 //var webBaseUrl = 'http://piwebuat.azurewebsites.net';
 
-var webBaseUrl = 'https://transportal-qa-web.azurewebsites.net';
+//var webBaseUrl = 'https://transportal-qa-web.azurewebsites.net';
 //var webBaseUrl = 'https://transportal-prod-web.azurewebsites.net';
 
